1c2691a3e4d1752f910a6e5fc1e33fa19f3facfe
John Murray <jmurrayufo@gmail.com>
2013-01-15 23:27:26 -0700

    Initial Upload

---
+++ d3goldlog.py
@@ -0,0 +1,38 @@
+import time
+import sys
+import datetime
+
+data = list()
+
+foo = raw_input("Initial Value:")
+
+data.append((time.time(),int(foo)))
+
+while(1):
+    foo=raw_input("Value: ")
+    if(foo == "-1"):
+        print "Delete last entry!"
+        data.pop()
+        continue
+    if(foo == ""):
+        print "Got Empty!"
+    else:
+        try: 
+            data.append((time.time(),int(foo)))
+        except ValueError:
+            print "Value \"",foo,"\" is not a number, it will be ignored..."
+            continue
+
+    print "Time Today:", datetime.timedelta(seconds=data[-1][0]-data[0][0])
+    print "dGold (total):",data[-1][1]-data[0][1]
+    print "dGold/Hr (total): %12.2f"%((data[-1][1]-data[0][1])/(data[-1][0]-data[0][0])*60*60)
+    if(len(data)>5):
+        print "dGold (last 5):",data[-1][1]-data[-5][1]
+        print "dGold/Hr (last 5): %12.2f"%((data[-1][1]-data[-5][1])/(data[-1][0]-data[-5][0])*60*60)
+    print "dGold (last):",data[-1][1]-data[-2][1]
+    print "dGold/Hr (last):  %12.2f"%((data[-1][1]-data[-2][1])/(data[-1][0]-data[-2][0])*60*60)
+    print "\n\n"
+
+
+
+
---
+++ lol.py
@@ -0,0 +1,84 @@
+import random
+ 
+items = [
+        ["Abyssal Scepter",                 2650],
+        ["Aegis of the Legion",             2150],
+        ["Atma's Impaler",                  2300],
+        ["Banner of Command",               2400],
+        ["Banshee's veil",                  2500],
+        ["Bilgewater Cultlass",             1925],
+        ["Executioner's calling",           1900],
+        ["Guardian Angel",                  2600],
+        ["Guinsoo's Rageblade",             2600],
+        ["Hextech Sweeper(dominion/TT)",    1920],
+        ["Kitae's Bloodrazor(dominion/tt)", 2525],
+        ["Last Whisper",                    2135],
+        ["Locket of the Iron Solari",       2000],
+        ["Malady",                          2035],
+        ["Manamune",                        2100],
+        ["Mikael's Crucible",               2200],
+        ["Morellonomicon",                  2200],
+        ["Nashor's tooth",                  2500],
+        ["Odyn's veil(Dominion/TT)",        2610],
+        ["Overlord's bloodmail",            2455],
+        ["Rod of Ages",                     2800],
+        ["Shard of True Ice",               1700],
+        ["Shurelya's Reverie",              2100],
+        ["Spirit Visage",                   2200],
+        ["Spirit of the Ancient Golem",     2400],
+        ["Spirit of the Elder Lizard",      2400],
+        ["Spirit of the Spectral Wraith",   2400],
+        ["Statikk Shiv",                    2500],
+        ["Sunfire Cape",                    2500],
+        ["Sword of the Divine",             2200],
+        ["The brutalizer",                  1337],
+        ["Thornmail",                       2200],
+        ["Tiamat",                          2300],
+        ["Twin Shadows",                    1900],
+        ["Void Staff",                      2295],
+        ["Warmog's Armor",                  2650],
+        ["Wicked Hatchet",                  2840],
+        ["Will of the Ancients",            2500],
+        ["Wit's end",                       2200],
+        ["Zeke's Herald",                   2450]
+        ]
+ 
+choices = [ "All Yordle", 
+            "All AD Carry",
+            "All Human",
+            "No Humans",
+            "No Mana",
+            "Alphabet",
+            "All Support",
+            "Fill Dorians",
+            "No Boots",
+            "No Consumables",
+            "Pick Item",
+            "SwapAKill",
+            "Keep It Full!",
+            "Max Lead",
+            "Deadly Aces",
+            "Unique Shoes"]
+ 
+result = random.choice(choices)
+
+result="Pick Item"
+ 
+print "Rules:",result
+if(result=="Alphabet"):
+    print "You must spell something out!"
+elif(result=="Fill Dorians"):
+    print "Only boots/dorians items until inventory is full, then may proceed with normal build"
+elif(result=="Pick Item"):
+    Item = random.choice(items)        
+    print "Everyone MUST build a(n) %s by %d minutes."%(Item[0], Item[1]/96+1+1.5+3)
+elif(result=="SwapAKill"):
+    print "Every time you get a kill, you you must swap to a new lane (your choice)"
+elif(result=="Keep It Full!"):
+    print "Never leave the summoners platform will more empty inventory spaces then when you got there."
+elif(result=="Max Lead"):
+    print "You may not push towers when the team is 5 or more kills ahead"
+elif(result=="Deadly Aces"):
+    print "If you score an ace, all alive players MUST rush the enemy summoners platform and die there if they reach it."
+elif(result=="Unique Shoes"):
+    print "Everyone must have a unique level 2+ set of boots, no duplicates!"
